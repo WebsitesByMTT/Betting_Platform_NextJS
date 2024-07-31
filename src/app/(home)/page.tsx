ce68ce5b0ae2c1b861da1480b982b38810e109fb
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-07-31 05:14:23 -0700

    Added: Footer Component, Fixed Crousal Autoplay,Ui

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,21 @@
+import Footer from "@/components/common/Footer";
 import Header from "@/components/common/Header";
 import Banner from "@/components/home/Banner";
 import BetCard from "@/components/home/BetCard";
-import Categorys from "@/components/home/Categorys";
+import Categories from "@/components/home/Categories";
 import QuickBet from "@/components/home/QuickBet";
 import TopBetsCarousel from "@/components/home/TopBetsCarousel";
 
 export default function Home() {
   return (
-    <main className=" w-[95%] pb-[4rem] p-2 mx-auto">
+    <main className=" w-[95%] p-2 mx-auto">
       <Header />
       <Banner />
-      <Categorys />
+      <Categories />
       <TopBetsCarousel />
       <QuickBet />
       <BetCard />
+      <Footer/>
     </main> 
   );
 }

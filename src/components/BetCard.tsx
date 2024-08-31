d553bdf9ea6e7c7bbff1063ac6136d40ffb83f90
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-08-31 17:29:42 +0530

    Updated: Date & Time format

--- src/components/BetCard.tsx
+++ src/components/BetCard.tsx
@@ -99,8 +99,18 @@ const BetCard: React.FC<any> = ({ betsData }) => {
         </div>
         <Favourite />
       </div>
-      <p className="text-[#67ffff] text-sm">
-        {leagues.commence_time.toLocaleString()}
+      <p className="text-[#67ffff] capitalize text-sm">
+        {new Date(leagues.commence_time).toLocaleDateString('en-US', {
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric'
+        })}{' '} | {' '}
+      
+        {new Date(leagues.commence_time).toLocaleTimeString([], {
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: true
+        })}
       </p>
       <div className="flex flex-col py-1.5 justify-between">
         <button className="grid grid-cols-5 space-x-2">
@@ -139,11 +149,10 @@ const BetCard: React.FC<any> = ({ betsData }) => {
       </div>
       <div className="flex gap-2 w-full">
         <button
-          className={`flex-1 py-2 rounded-lg text-sm disabled:bg-[#27252A] disabled:border-[#4A484D] disabled:cursor-not-allowed transition-colors border-[1px] flex justify-between px-2 ${
-            isBetInAllBets("home_team" + betsData.id + betsData.markets[0]?.key)
+          className={`flex-1 py-2 rounded-lg text-sm disabled:bg-[#27252A] disabled:border-[#4A484D] disabled:cursor-not-allowed transition-colors border-[1px] flex justify-between px-2 ${isBetInAllBets("home_team" + betsData.id + betsData.markets[0]?.key)
               ? "bg-gradient-to-b from-[#82ff606a] to-[#4f993a6d] border-[#82FF60] shadow-inner"
               : "bg-[#040404] border-transparent"
-          }`}
+            }`}
           onClick={() => {
             handleBet("home_team", betsData);
           }}
@@ -160,11 +169,10 @@ const BetCard: React.FC<any> = ({ betsData }) => {
           </p>
         </button>
         <button
-          className={`flex-1 py-2 rounded-lg text-sm disabled:bg-[#27252A] disabled:border-[#4A484D] disabled:cursor-not-allowed transition-colors border-[1px] flex justify-between px-2 ${
-            isBetInAllBets("away_team" + betsData.id + betsData.markets[0]?.key)
+          className={`flex-1 py-2 rounded-lg text-sm disabled:bg-[#27252A] disabled:border-[#4A484D] disabled:cursor-not-allowed transition-colors border-[1px] flex justify-between px-2 ${isBetInAllBets("away_team" + betsData.id + betsData.markets[0]?.key)
               ? "bg-gradient-to-b from-[#82ff606a] to-[#4f993a6d] border-[#82FF60] shadow-inner"
               : "bg-[#040404] border-transparent "
-          }`}
+            }`}
           onClick={() => {
             handleBet("away_team", betsData);
           }}

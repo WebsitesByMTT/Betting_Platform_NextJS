af0bbbf53ed8fdcbaa46a2af34381947e595ae08
rohit <rohit@underpinservices.com>
2024-09-17 11:44:10 +0530

    fix: on redeem 2 toast msgs

--- src/components/MyBets.tsx
+++ src/components/MyBets.tsx
@@ -89,7 +89,7 @@ const MyBets = () => {
       fetchBet();
       return toast.error(response.error);
     }
-    toast.success(response?.responseData?.message);
+    // toast.success(response?.responseData?.message);
     fetchBet();
   };
 
@@ -129,9 +129,8 @@ const MyBets = () => {
               <div className="h-full px-1 rounded-tl-lg rounded-bl-lg bg-gradient-to-b from-[#ECB800] to-[#58565D00]"></div>
             )}
             <p
-              className={`text-sm lg:text-lg font-medium whitespace-nowrap py-1 ${
-                selectedOption === item ? "px-3" : "px-5"
-              } capitalize `}
+              className={`text-sm lg:text-lg font-medium whitespace-nowrap py-1 ${selectedOption === item ? "px-3" : "px-5"
+                } capitalize `}
             >
               {item}
             </p>
@@ -164,20 +163,18 @@ const MyBets = () => {
                     <tr
                       id={item._id}
                       key={`${item._id}-${dataIndex}-single`}
-                      className={`text-center font-extralight hover:bg-[#8585851A]  border-[#414141] ${
-                        data.status === "redeem"
+                      className={`text-center font-extralight hover:bg-[#8585851A]  border-[#414141] ${data.status === "redeem"
                           ? "bg-[#121216]"
                           : " bg-gradient-to-b from-[#1c1a2176] to-[#0d0c156d]"
-                      } border-t-[1px] transition-all duration-500 `}
+                        } border-t-[1px] transition-all duration-500 `}
                     >
                       <td className="w-[20%] py-2 md:py-4">
                         <div className="w-full flex flex-col gap-1 px-3">
                           <span
-                            className={`${
-                              data.status === "redeem"
+                            className={`${data.status === "redeem"
                                 ? "text-[#55545a]"
                                 : "text-white"
-                            } font-medium  text-left text-sm md:text-lg`}
+                              } font-medium  text-left text-sm md:text-lg`}
                           >
                             {data.sport_title}
                           </span>
@@ -185,16 +182,14 @@ const MyBets = () => {
                             <span
                               className={
                                 data.bet_on === "home_team"
-                                  ? `${
-                                      data.status === "redeem"
-                                        ? "text-[#57555f]"
-                                        : "text-[#FFC400]"
-                                    }`
-                                  : `${
-                                      data.status === "redeem"
-                                        ? "text-[#424149]"
-                                        : "text-white"
-                                    }`
+                                  ? `${data.status === "redeem"
+                                    ? "text-[#57555f]"
+                                    : "text-[#FFC400]"
+                                  }`
+                                  : `${data.status === "redeem"
+                                    ? "text-[#424149]"
+                                    : "text-white"
+                                  }`
                               }
                             >
                               {data.home_team.name}
@@ -211,61 +206,54 @@ const MyBets = () => {
                             <span
                               className={
                                 data.bet_on === "away_team"
-                                  ? `${
-                                      data.status === "redeem"
-                                        ? "text-[#7b7984]"
-                                        : "text-[#FFC400]"
-                                    }`
-                                  : `${
-                                      data.status === "redeem"
-                                        ? "text-[#424149]"
-                                        : "text-white"
-                                    }`
+                                  ? `${data.status === "redeem"
+                                    ? "text-[#7b7984]"
+                                    : "text-[#FFC400]"
+                                  }`
+                                  : `${data.status === "redeem"
+                                    ? "text-[#424149]"
+                                    : "text-white"
+                                  }`
                               }
                             >
                               {data.away_team.name}
                             </span>
                           </span>
                           <span
-                            className={`text-[9px] md:text-[11px] p-1  border-[1px] ${
-                              data.status === "redeem"
+                            className={`text-[9px] md:text-[11px] p-1  border-[1px] ${data.status === "redeem"
                                 ? "bg-[#17161f] text-[#56555d] border-[#353342]"
                                 : "bg-[#303030] text-[#A1A1A1] border-[#414141] "
-                            }  rounded-lg w-fit`}
+                              }  rounded-lg w-fit`}
                           >
                             {formatDateTime(data.commence_time)}
                           </span>
                         </div>
                       </td>
                       <td
-                        className={`${
-                          data.status === "redeem" ? "text-[#555458]" : ""
-                        } text-sm md:text-lg`}
+                        className={`${data.status === "redeem" ? "text-[#555458]" : ""
+                          } text-sm md:text-lg`}
                       >
                         $ {item.amount}
                       </td>
                       <td
-                        className={`uppercase text-sm md:text-lg ${
-                          data.status === "redeem" ? "text-[#555458]" : ""
-                        }`}
+                        className={`uppercase text-sm md:text-lg ${data.status === "redeem" ? "text-[#555458]" : ""
+                          }`}
                       >
                         {data.market}
                       </td>
                       <td className="text-sm md:text-lg">
                         <div className="flex flex-col gap-2">
                           <span
-                            className={`${
-                              data.status === "redeem"
+                            className={`${data.status === "redeem"
                                 ? "text-[#403f4b]"
                                 : "text-gray-400"
-                            } text-sm`}
+                              } text-sm`}
                           >
                             {data.oddsFormat}
                           </span>
                           <span
-                            className={`${
-                              data.status === "redeem" ? "text-[#555458]" : ""
-                            }`}
+                            className={`${data.status === "redeem" ? "text-[#555458]" : ""
+                              }`}
                           >
                             {data.bet_on === "away_team"
                               ? data.away_team.odds
@@ -274,29 +262,26 @@ const MyBets = () => {
                         </div>
                       </td>
                       <td
-                        className={`${
-                          data.status === "redeem" ? "text-[#555458]" : ""
-                        } text-sm md:text-lg`}
+                        className={`${data.status === "redeem" ? "text-[#555458]" : ""
+                          } text-sm md:text-lg`}
                       >
                         {item.possibleWinningAmount.toFixed(3)}
                       </td>
                       <td
-                        className={`text-sm ${
-                          data.status === "redeem"
+                        className={`text-sm ${data.status === "redeem"
                             ? "text-gray-500"
                             : "text-[#FF6A00]"
-                        } md:text-lg capitalize `}
+                          } md:text-lg capitalize `}
                       >
                         {data.status}
                       </td>
                       <td>
                         <button
                           disabled={data.status !== "pending"}
-                          className={` px-4 py-1 rounded-lg text-sm md:text-lg ${
-                            data.status !== "pending"
+                          className={` px-4 py-1 rounded-lg text-sm md:text-lg ${data.status !== "pending"
                               ? "text-gray-500"
                               : "text-[#00C8FF] bg-white bg-opacity-10"
-                          }`}
+                            }`}
                           onClick={() => {
                             setOpen(true);
                             setBetID(item._id);
@@ -316,26 +301,22 @@ const MyBets = () => {
                       <tr
                         id={item._id}
                         key={`${item._id}-${dataIndex}-combo`}
-                        className={`${
-                          dataIndex === 0 ? "border-t-[1px]" : ""
-                        } text-center font-extralight border-[#f3aa3589] border-x-[1px] border-b-[1px] ${
-                          dataIndex === item.data.length - 1
+                        className={`${dataIndex === 0 ? "border-t-[1px]" : ""
+                          } text-center font-extralight border-[#f3aa3589] border-x-[1px] border-b-[1px] ${dataIndex === item.data.length - 1
                             ? "border-b-[#d8d2d2a3]"
                             : "border-b-[#414141]"
-                        }  hover:bg-[#8585851A] ${
-                          data.status === "redeem"
+                          }  hover:bg-[#8585851A] ${data.status === "redeem"
                             ? "bg-[#121216]"
                             : " bg-gradient-to-b from-[#1c1a2176] to-[#0d0c156d]"
-                        }`}
+                          }`}
                       >
                         <td className="w-[20%] py-4">
                           <div className="w-full flex flex-col gap-1 px-3">
                             <span
-                              className={`${
-                                data.status === "redeem"
+                              className={`${data.status === "redeem"
                                   ? "text-[#55545a]"
                                   : "text-white"
-                              } font-medium text-left text-sm md:text-lg`}
+                                } font-medium text-left text-sm md:text-lg`}
                             >
                               {data.sport_title}
                             </span>
@@ -343,16 +324,14 @@ const MyBets = () => {
                               <span
                                 className={
                                   data.bet_on === "home_team"
-                                    ? `${
-                                        data.status === "redeem"
-                                          ? "text-[#57555f]"
-                                          : "text-[#FFC400]"
-                                      }`
-                                    : `${
-                                        data.status === "redeem"
-                                          ? "text-[#424149]"
-                                          : "text-white"
-                                      }`
+                                    ? `${data.status === "redeem"
+                                      ? "text-[#57555f]"
+                                      : "text-[#FFC400]"
+                                    }`
+                                    : `${data.status === "redeem"
+                                      ? "text-[#424149]"
+                                      : "text-white"
+                                    }`
                                 }
                               >
                                 {data.home_team.name}
@@ -369,27 +348,24 @@ const MyBets = () => {
                               <span
                                 className={
                                   data.bet_on === "away_team"
-                                    ? `${
-                                        data.status === "redeem"
-                                          ? "text-[#57555f]"
-                                          : "text-[#FFC400]"
-                                      }`
-                                    : `${
-                                        data.status === "redeem"
-                                          ? "text-[#424149]"
-                                          : "text-white"
-                                      }`
+                                    ? `${data.status === "redeem"
+                                      ? "text-[#57555f]"
+                                      : "text-[#FFC400]"
+                                    }`
+                                    : `${data.status === "redeem"
+                                      ? "text-[#424149]"
+                                      : "text-white"
+                                    }`
                                 }
                               >
                                 {data.away_team.name}
                               </span>
                             </span>
                             <span
-                              className={`text-[9px] md:text-[11px] p-1 ${
-                                data.status === "redeem"
+                              className={`text-[9px] md:text-[11px] p-1 ${data.status === "redeem"
                                   ? "bg-[#17161f] text-[#56555d] border-[#353342]"
                                   : "bg-[#303030] text-[#A1A1A1] border-[#414141] "
-                              } border-[1px]  rounded-lg w-fit`}
+                                } border-[1px]  rounded-lg w-fit`}
                             >
                               {formatDateTime(data.commence_time)}
                             </span>
@@ -399,27 +375,24 @@ const MyBets = () => {
                           --/--
                         </td>
                         <td
-                          className={`uppercase text-sm md:text-lg ${
-                            data.status === "redeem" ? "text-[#555458]" : ""
-                          }`}
+                          className={`uppercase text-sm md:text-lg ${data.status === "redeem" ? "text-[#555458]" : ""
+                            }`}
                         >
                           {data.market}
                         </td>
                         <td className="text-sm md:text-lg">
                           <div className="flex flex-col gap-2">
                             <span
-                              className={`text-sm ${
-                                data.status === "redeem"
+                              className={`text-sm ${data.status === "redeem"
                                   ? "text-[#403f4b]"
                                   : "text-gray-400"
-                              }`}
+                                }`}
                             >
                               {data.oddsFormat}
                             </span>
                             <span
-                              className={`${
-                                data.status === "redeem" ? "text-[#555458]" : ""
-                              }`}
+                              className={`${data.status === "redeem" ? "text-[#555458]" : ""
+                                }`}
                             >
                               {data.bet_on === "away_team"
                                 ? data.away_team.odds
@@ -431,11 +404,10 @@ const MyBets = () => {
                           --/--
                         </td>
                         <td
-                          className={`text-sm ${
-                            data.status === "redeem"
+                          className={`text-sm ${data.status === "redeem"
                               ? "text-gray-500"
                               : "text-[#FF6A00]"
-                          }  md:text-lg capitalize `}
+                            }  md:text-lg capitalize `}
                         >
                           {data.status}
                         </td>
@@ -445,42 +417,38 @@ const MyBets = () => {
                     <tr className="text-center font-extralight bg-gradient-to-b from-[#1c1a2176] to-[#0d0c156d] border-[1px] border-[#f3aa357c]">
                       <td className="py-3"></td>
                       <td
-                        className={`py-3 text-lf ${
-                          item.status === "redeem"
+                        className={`py-3 text-lf ${item.status === "redeem"
                             ? "text-[#55545a]"
                             : "text-white"
-                        }`}
+                          }`}
                       >
                         $ {item.amount}
                       </td>
                       <td className="py-3"></td>
                       <td className="py-3"></td>
                       <td
-                        className={`py-3 text-lf ${
-                          item.status === "redeem"
+                        className={`py-3 text-lf ${item.status === "redeem"
                             ? "text-[#55545a]"
                             : "text-white"
-                        }`}
+                          }`}
                       >
                         {item.possibleWinningAmount.toFixed(3)}
                       </td>
                       <td
-                        className={`text-sm ${
-                          item.status === "redeem"
+                        className={`text-sm ${item.status === "redeem"
                             ? "text-gray-500"
                             : "text-[#FF6A00]"
-                        } py-3 md:text-lg capitalize `}
+                          } py-3 md:text-lg capitalize `}
                       >
                         {item.status}
                       </td>
                       <td className="py-3">
                         <button
                           disabled={item.status !== "pending"}
-                          className={` px-4 py-1 rounded-lg text-sm md:text-lg ${
-                            item.status !== "pending"
+                          className={` px-4 py-1 rounded-lg text-sm md:text-lg ${item.status !== "pending"
                               ? "text-gray-500"
                               : "text-[#00C8FF] bg-white  bg-opacity-10"
-                          }`}
+                            }`}
                           onClick={() => {
                             setOpen(true);
                             setBetID(item._id);
